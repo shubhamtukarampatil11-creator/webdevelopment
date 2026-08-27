ccd8be3ea54d9df76227b5d1d7d87f7a5cb81f2e
shubham patil <shubhamtukarampatil11@gmail.com>
2026-08-27 11:29:01 +0530

    Add files via upload

---
+++ function.js
@@ -0,0 +1,23 @@
+let studentName ="Shubham";
+let age = 20;
+let marks = 85;
+
+console.log(studentName);
+console.log(age);
+console.log(marks);
+
+let name = prompt("enter  your name");
+alert(name);
+console.log(name);
+
+let userAge = prompt("Enter you age");
+console.log(userAge);
+
+let isConfirmed = confirm("Do you want to continue");
+console.log(isConfirmed);
+// console.log(confirm);
+
+let userName = prompt("what is you name");
+document.writeln(" hello " + userName);
+
+
